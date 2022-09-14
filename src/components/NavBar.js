c082fc33fabd312857100cece8c9c99f8b325a6c
Javier-Garcia-Estrada <javiergarciaestrada7@gmail.com>
2022-09-14 18:34:47 -0400

    update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -44,9 +44,9 @@ export const NavBar = () => {
           </Nav>
           <span className="navbar-text">
             <div className="social-icon">
-                <a href="#"><img src={""} alt="" /></a>
-                <a href="#"><img src={""} alt="" /></a>
-                <a href="#"><img src={""} alt="" /></a>
+                <a href="#"><img src={navIcon1} alt="" /></a>
+                <a href="#"><img src={navIcon2} alt="" /></a>
+                <a href="#"><img src={navIcon3} alt="" /></a>
             </div>
             <button className="vvd" onClick={() => console.log("connect")}><span>Let's Connect</span></button>
           </span>
@@ -56,4 +56,4 @@ export const NavBar = () => {
     )
 }
 
-//ended at 14:12
+//ended at 18:30
