77fd139127d1c77e4f5cd7bdd57ed7a5e55fe6cb
Javier-Garcia-Estrada <javiergarciaestrada7@gmail.com>
2022-09-20 21:16:58 -0400

    Finished Banner section and Added/Finished Skills section as well

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Banner } from "./components/Banner.js";
 import { NavBar } from "./components/NavBar";
+import { Skills } from "./components/Skills"
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
     <div className="App">
       <NavBar />
       <Banner />
-
+      <Skills />
     </div>
   );
 }
