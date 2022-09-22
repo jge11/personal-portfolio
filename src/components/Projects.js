0764f51c70aed34ca76fbd4ac987bf648c3d2fa8
Javier-Garcia-Estrada <javiergarciaestrada7@gmail.com>
2022-09-21 21:41:56 -0400

    Added Projects Component

---
+++ src/components/Projects.js
@@ -0,0 +1,55 @@
+
+
+export const Projects = () => {
+
+    const projects = [
+    {
+        title: "Buisness Startup",
+        description:" Design & Development",
+        imgUrl: projImg1,
+    },
+    {
+        title: "Buisness Startup",
+        description:" Design & Development",
+        imgUrl: projImg1,
+    },
+    {
+        title: "Buisness Startup",
+        description:" Design & Development",
+        imgUrl: projImg1,
+    },
+    {
+        title: "Buisness Startup",
+        description:" Design & Development",
+        imgUrl: projImg1,
+    },
+    {
+        title: "Buisness Startup",
+        description:" Design & Development",
+        imgUrl: projImg1,
+    },
+    {
+        title: "Buisness Startup",
+        description:" Design & Development",
+        imgUrl: projImg1,
+    },
+
+    ]
+    return (
+        <section className="project" id="project">
+            <Container>
+                <Row>
+                    <Col>
+                        <h2>Projects</h2>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni laborum excepturi sunt maxime, nesciunt accusantium? Consequatur totam atque sed perspiciatis, ratione aspernatur praesentium nesciunt repudiandae nihil, laboriosam odio, ullam quod!
+                        </p>
+                        <Nav variant="pills" defaultActiveKey="/home">
+                            
+                        </Nav>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
+}
