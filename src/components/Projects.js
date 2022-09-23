27a047ad69eb63a4e1404d1f28e114c3d5ac7f88
Javier-Garcia-Estrada <javiergarciaestrada7@gmail.com>
2022-09-22 20:36:38 -0400

    Updated Projects Components

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,3 +1,4 @@
+import { Nav, Tab, TabContainer } from "react-bootstrap"
 
 
 export const Projects = () => {
@@ -44,9 +45,36 @@ export const Projects = () => {
                         <p>
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni laborum excepturi sunt maxime, nesciunt accusantium? Consequatur totam atque sed perspiciatis, ratione aspernatur praesentium nesciunt repudiandae nihil, laboriosam odio, ullam quod!
                         </p>
+                        <Tab.Container id="projects-tab" defaultActiveKey="first">
                         <Nav variant="pills" defaultActiveKey="/home">
-                            
+                            <Nav.Item>
+                                <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                            </Nav.Item>
                         </Nav>
+                        <Tab.Content>
+                            <Tab.Pane eventKey="first">
+                                <Row>
+                                    {
+                                        projects.map((project, index) =>{
+                                            return(
+                                                <p>{project.title}</p>
+                                            )
+                                        }
+                                        
+                                        )
+                                    }
+                                </Row>
+                            </Tab.Pane>
+                            <Tab.Pane eventKey="second"></Tab.Pane>
+                            <Tab.Pane eventKey="third"></Tab.Pane>
+                        </Tab.Content>
+                        </Tab.Container>
                     </Col>
                 </Row>
             </Container>
